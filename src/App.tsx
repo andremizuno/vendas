ce424a39270d9d821e01896a18e650e55f860196
andremizuno <andremizuno@gmail.com>
2023-09-03 16:07:15 -0300

    Olá mundo

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { SafeAreaView, Text } from "react-native";
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Olá Mundo!</Text>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
